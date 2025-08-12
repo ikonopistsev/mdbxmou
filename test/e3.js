73ca2a6ea98a3b572763de3aa32f90202af40410
meil <ikonopistsev@roboforex.com>
2025-08-12 09:30:08 +0300

    e3

--- test/e3.js
+++ test/e3.js
@@ -2,33 +2,31 @@
 
 const fs = require('fs');
 const MDBX = require('../lib/nativemou.js');
-const { MDBX_Env, MDBX_db_flag, MDBX_env_flag } = MDBX;
+const { MDBX_Env, MDBX_Param } = MDBX;
 
 const test = async () => {
   const db_dir = 'e3';
 
   await fs.promises.rm(db_dir, { recursive: true, force: true });
 
-  console.log(MDBX_env_flag);
+  console.log("MDBX_Param:", MDBX_Param);
+  // получаем константы
+  const { key_mode, key_flag, query_mode, value_flag } = MDBX_Param;
 
   const db = new MDBX_Env();
 
   console.log('Opening database...');
 
   await db.open({
-      path: db_dir,
-      flag: MDBX_env_flag.MDBX_ACCEDE|
-        MDBX_env_flag.MDBX_LIFORECLAIM|MDBX_env_flag.MDBX_UTTERLY_NOSYNC
+      path: db_dir
     });
 
   console.log('Start write');
-  const db_flag = BigInt(
-    MDBX_db_flag.MDBX_CREATE|MDBX_db_flag.MDBX_INTEGERKEY);
   const count = 100000;
   for (let i = 0; i < count; i++) {
     const txn = db.startWrite();
-    const dbi = txn.getDbi(db_flag);
-    dbi.put(BigInt(i), `value_${i}`);
+    const dbi = txn.createMap(key_mode.ordinal);
+    dbi.put(i, `value_${i}`);
     txn.commit();
   }
   
